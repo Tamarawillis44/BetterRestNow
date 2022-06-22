a8ac1a236e89acd80beb119d927ea98102fe2dc2
Tamarawillis44 <techietl444@gmail.com>
2022-06-21 22:07:46 -0400

    Initial Commit

---
+++ BetterRestNow/BetterRestNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestNowApp.swift
+//  BetterRestNow
+//
+//  Created by Tamara laraque on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
